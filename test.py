e7e39c3d4524f4283530dd644e9ae9238e86993f
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-12-06 19:29:09 +0530

    small changes

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import random
 import logging
 
 import cv2
+import pandas as pd
 import pytesseract as ts
 from difflib import SequenceMatcher
 
 from predict import load_model, process_image
 from utils import load_annotation, get_image_paths
 
-MODEL_PATH = 'models/model_synth_funsd2.h5'
-# DIR_NAME = '../../FUNSD_TEXT_RECOGNITION/test_data'
+MODEL_PATH = 'models/model_synth_2_10.h5'
 DIR_NAME = 'data/synth_test_1'
 
 
@@ -18,6 +18,8 @@ DIR_NAME = 'data/synth_test_1'
 
 
 def similarity_score(a, b):
+    if a is None or b is None:
+        return 0
     a = a.lower()
     b = b.lower()
     return SequenceMatcher(None, a, b).ratio()
@@ -27,18 +29,14 @@ def eval_image(img_fn, crnn):
     img = cv2.imread(img_fn)
 
     true_text = load_annotation(img_fn)
-    if true_text is None:
-        true_text = ''
     ts_text = ts.image_to_string(img, config=("-l eng --oem 1 --psm 8"))
     crnn_text = process_image(crnn.model, [img])[0]
 
-    res = (
-        true_text,
-        (ts_text, similarity_score(true_text, ts_text)),
-        (crnn_text, similarity_score(true_text, crnn_text)),
-    )
-
-    return res
+    return {
+        'true_text': true_text,
+        'tesseract': ts_text,
+        'crnn': crnn_text,
+    }
 
 
 def main():
@@ -48,23 +46,28 @@ def main():
     images = get_image_paths(root)
     random.shuffle(images)
 
+    rec = []
+
     ts_score = 0
     crnn_score = 0
     for n, image in enumerate(images, 1):
-        res = eval_image(image, crnn)
-        ts_score += res[1][1]
-        crnn_score += res[2][1]
+        result = eval_image(image, crnn)
+        ts_score += similarity_score(result['true_text'], result['tesseract'])
+        crnn_score += similarity_score(result['true_text'], result['crnn'])
+        rec.append({'image': image, 'true_text': result['true_text'], 'tesseract': result['tesseract'], 'crnn': result['crnn']})
 
-        print(image)
-        print(res)
-        print(ts_score/n, crnn_score/n)
+        print(n, image)
+        print(result)
+        print(ts_score / n, crnn_score / n)
         print()
 
-        time.sleep(2)
-        # break
+        # time.sleep(2)
 
+    rec_df = pd.DataFrame(rec)
+    rec_df.to_csv('records_10.csv')
 
-# %%
+    # %%
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.ERROR)
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from keras.optimizers import Adadelta
 from data_generator import DataGenerator
 from model import CRNN
 
-TRAIN_DIR_NAME = '../../synth_words_num'
+TRAIN_DIR_NAME = '../../FUNSD_TEXT_RECOGNITION/train_data'
 
 
 def main():
@@ -16,16 +16,16 @@ def main():
     ada = Adadelta()
     crnn.model.compile(loss=lambda y_true, y_pred: y_pred, optimizer=ada)
 
-    batch_size = 128
+    batch_size = 64
     train_gen = DataGenerator(data_path=TRAIN_DIR_NAME, batch_size=batch_size)
 
     os.system('mkdir -p models')
-    early_stop = EarlyStopping(monitor='loss', min_delta=0.01, patience=4, mode='min', verbose=1)
+    early_stop = EarlyStopping(monitor='loss', min_delta=0.01, patience=8, mode='min', verbose=1)
     checkpoint = ModelCheckpoint(filepath='models/model_curr_{epoch:02d}_{loss:.3f}.h5', monitor='loss', verbose=1,
                                  mode='min', period=1, save_weights_only=True)
 
     # load previous checkpoints
-    # crnn.model.load_weights('models/model_84_0.137.h5')
+    crnn.model.load_weights('models/model_synth_2_35.h5')
 
     crnn.model.fit_generator(
         generator=train_gen,
